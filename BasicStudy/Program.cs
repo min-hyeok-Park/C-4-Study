6858b167511a1a85731df4bc004865e507df6662
Min hyeok Park <mhpark1114@gmail.com>
2022-01-07 22:25:51 +0900

    초기 커밋.

---
+++ BasicStudy/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BasicStudy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("{0} {1}", 3.14f, 12);
+            Console.WriteLine("{0} + {1} = {2}", 1, 2, 1 + 2);
+            Console.WriteLine("{0:C} {1:P} {0:X}", 123, 123.45, 123);
+        }
+    }
+}
